6a308a42c99679275304b8005bc7c1315fca6d8a
danplo1 <budweiser401@wp.pl>
2019-03-05 13:33:11 +0100

    Initial commit

---
+++ src/main/java/pl/danplo/springsecurity/Hello.java
@@ -0,0 +1,15 @@
+package pl.danplo.springsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+       return "hello!";
+
+    }
+}
